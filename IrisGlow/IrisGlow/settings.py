848ddeb5e33ba862e99011047f00507d1e2e3a87
Akku Shaji <akkushaji2024a@mca.ajce.in>
2023-09-26 13:46:57 +0530

    Commit6

--- IrisGlow/IrisGlow/settings.py
+++ IrisGlow/IrisGlow/settings.py
@@ -32,15 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'IrisGlowApp',
-    'jazzmin',
-    'django.contrib'
+    'IrisGlowApp',   
 ]
 
 MIDDLEWARE = [
